ff53390421730e9a24c1b510c1c16070fdd53c71
sathish <rksathish09@gmail.com>
2022-04-20 16:53:06 +0200

    check abs value of tolerance and add kernelized k-means

--- Sathish/clustering.py
+++ Sathish/clustering.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+
+def polynomial_kernel(x1, x2):
+    return (x1.T * x2)**2
+
 def K_means_clustering(data, clusters):
     '''
     Cluster data using K-means clustering algorithm
@@ -23,7 +27,12 @@ def K_means_clustering(data, clusters):
     tol = 1
     previous_tol = 2
 
-    while tol > 0 and (previous_tol - tol) > 0.0:
+    no_of_iterations = 0
+
+    while np.abs(tol) > 0 and np.abs(previous_tol - tol) > 0.0:
+
+        no_of_iterations += 1
+        
         distance_to_centers = np.zeros((len(data), len(centers)))
 
         for i, center in enumerate(centers):
@@ -45,4 +54,52 @@ def K_means_clustering(data, clusters):
             centers.append(sum(data[clusters[i]])/len(clusters[i]))
             tol += sum((data[clusters[i]] - centers[i])**2)
 
+    print("Converged in: %d iterations", no_of_iterations)
+
+    return clusters
+
+def kernelized_k_means_clustering(data, clusters, kernel = polynomial_kernel):
+
+    '''
+    
+    Cluster data using kernelized K-means clustering algorithm
+
+    data: Input data as flattened (N x 1 array)
+    clusters: No. of clusters (dict); keys represents the label of clusters and values are indices
+    kernel: A kernel function of your own. Default is polynomial kernel.
+
+    Output:
+    -------
+    Clustered data (dict); keys are the cluster labels and values are the pixel index belonging to a cluster
+
+    
+    '''
+    distance_to_centers = np.zeros((len(data), len(clusters)))
+
+    tol = 1
+    previous_tol = 2
+
+    A = kernel(data, data)
+
+    no_of_iterations = 0
+
+    while np.abs(tol) > 0 and np.abs(previous_tol - tol) > 0.0:
+        no_of_iterations += 1
+
+        for cluster_label, cluster in clusters.items():
+            B = np.array([2/len(cluster) * sum(kernel(x, data[cluster])) for x in data])
+            C = 1/(len(cluster)**2) * sum(kernel(data[cluster], data[cluster]))
+            distance_to_centers[:, cluster_label] = A - B + C
+
+        minimum_distance = np.argmin(distance_to_centers, axis = 1)
+
+        previous_tol = tol
+        tol = sum([distance[minimum_distance[i]] for i, distance in enumerate(distance_to_centers)])
+
+        for i, dist in enumerate(np.unique(minimum_distance)):
+            clusters[dist] = np.where(minimum_distance == dist)[0]
+        
+          
+    print("Converged in: %d iterations", no_of_iterations)
+    
     return clusters
