54d57d840022816d75e466f0e367930785b41283
sofigami <sofiakotti@gmail.com>
2022-04-28 10:23:26 +0300

    Final script Kmeans

--- Sofia/clustering.py
+++ Sofia/clustering.py
@@ -17,20 +17,25 @@ import matplotlib.pyplot as plt
 # from scipy import io
 from sklearn import metrics
 
+# Import dataset
 from sklearn.datasets import fetch_openml
 X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 
 #%%
 t2 = time.time()
+# Convert data to arrays
 Xnp = pd.DataFrame.to_numpy(X)
 ynp = pd.DataFrame.to_numpy(y)
-# Xnew = Xnp[:10000, :]
-# ynew = ynp[:10000]
-Xnew = Xnp
-ynew = ynp
+
+# Choose number of samples to work with
+choose_num_samples = 70000
+Xnew = Xnp[:choose_num_samples, :]
+ynew = ynp[:choose_num_samples]
+
+# Normalize features
 Xnew = Xnew / 255;
 
-#%%
+#%% The two necessary functions for kMeans
 def assign_to_cluster(X, C):
     num_features = np.shape(X)[1]
     #print(num_features)
@@ -51,6 +56,7 @@ def update_centroids(X, idx, K):
         C[idx_c, :] = np.mean(X[np.where(idx == idx_c)[0], :], axis = 0)
        # print(C)
     return C
+
 #%% Function to find the most frequent number per cluster
 # source: https://www.geeksforgeeks.org/frequent-element-array/
 def mostFrequent(arr):
@@ -73,18 +79,16 @@ def mostFrequent(arr):
          
     return res, max_count
 #%%
+K = 10 # Number of clusters
 [num_samples, num_features] = np.shape(Xnew)
 
-
-K = 10
-
 #%% Initialize centroids
-randidx = rnd.permutation(num_samples)[:K]
-# randidx = np.arange(K)
-centroids = Xnew[randidx,:] #initial_centroids
+randidx = rnd.permutation(num_samples)[:K]  # randomly
+# randidx = np.arange(K)        # take the first K samples as initial centroids
+centroids = Xnew[randidx,:] 
 
 #%%
-max_iter = 50
+max_iter = 50    # Maximum number of iterations
 iter = 0
 diff_obj_f = -1
 idx_sample = np.arange(num_samples)
@@ -101,31 +105,28 @@ while iter < max_iter and diff_obj_f < 0:
     iter  += 1
     # print(idx)
     
+# # Plot resulting centroids
+# for idx_c in np.arange(K):
+#     Ximage = np.resize(centroids[idx_c, :], (28, 28))
+#     imgplot = plt.imshow(Ximage)
+#     plt.show()
+    
 #%%
 t3 = time.time()
 print('Time elapsed with data loading [s]: ', t3 - t1)
 print('Time elapsed without data loading [s]: ', t3 - t2)
 
-# Plot resulting centroids
-# for idx_c in np.arange(K):
-#     Ximage = np.resize(centroids[idx_c, :], (28, 28))
-#     imgplot = plt.imshow(Ximage)
-#     plt.show()
-
-#%%
-# a = 0
-# for idx_c in np.arange(K): 
-#     a += sum(idx == idx_c)
-#%% Accuracy calculation
+#%% Accuracy calculation as in assignment description
 m = np.zeros(K)
 A = np.zeros(K)
 freq_m = np.zeros(K)
 for idx_c in np.arange(K):
-    m[idx_c], freq_m[idx_c] = mostFrequent(ynew[idx == idx_c])
+    freq_m[idx_c] = mostFrequent(ynew[idx == idx_c])[1]
     A[idx_c] = freq_m[idx_c]
     
 Acc = sum(A) / num_samples
+
+#%% # Show metrics
 print('Accuracy: ', Acc)
 print('Completeness: ', metrics.completeness_score(ynew, idx))
 print('Homogeneity: ', metrics.homogeneity_score(ynew, idx))
-print('Acc2: ', metrics.accuracy_score(ynew, idx))
